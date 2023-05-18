dc76dab371534e8a4b9253b717a59cf45b82d7aa
Damon Murdoch <damonmurdoch1@gmail.com>
2023-05-19 00:37:49 +1000

    Revert "Added termcolor to requirements"
    This reverts commit 8685319fb0ee10f1a4765aeb07d858ee63f42b1e.
    
    This commit did a whole lot more
    (and broke a whole lot more)
    than it said it did :)

--- config.example.py
+++ config.example.py
@@ -1,16 +1,14 @@
 # File Configuration
 LOG_FILE = 'forwarder.log'
 
-# List of paths to monitor, and the channels to forward to
-MONITOR = [
-    {
-        "path": "your_file_or_folder",
-        "channel": 0
-    }
-]
+# Path(s) to check for new files
+MONITOR_PATH = "path_to_files"
 
 # Seconds between checks
 MONITOR_DELAY = 5
 
 # API Token
 DISCORD_TOKEN = 'discord_api_token'
+
+# Channel ID
+DISCORD_CHANNEL = 0
